2768491dd9ef7c456746ccc5e67dc819047208aa
Magnus P. Nytun <MagnusNytun92@gmail.com>
2022-04-30 13:28:13 +0200

    working on torch only version of trainer for qdl

--- assignment-2/qdl_replay_experience.py
+++ assignment-2/qdl_replay_experience.py
@@ -95,10 +95,108 @@ class QDeepLearningExpReplay(AgentQLearning):
             plt.plot(losses)
 
 
+    def train_torch(self, epochs=1000, mem_size=1000, batch_size=200, max_num_games=150):
+        losses = []
+        epsilon = self.max_eps
+
+        state_batch = torch.empty((batch_size, self.state_dim))
+        next_state_batch = torch.empty((batch_size, self.state_dim))
+
+        action_batch = torch.empty((batch_size, 1))
+        reward_batch = torch.empty((batch_size, 1))
+        done_batch = torch.empty((batch_size, 1))
+
+        c = 0
+
+        for _ in tqdm(range(epochs)):
+            state = torch.from_numpy(self.env.reset()).to(self.device).float()
+            done = False
+
+            for g in range(max_num_games):
+                q_vals = self.model(state)
+
+                if np.random.random() < epsilon:
+                    action = np.random.choice(self.n_actions)
+                else:
+                    action = torch.argmax(q_vals).item()
+
+                next_state, reward, done, _ = self.env.step(action)
+
+                if c < batch_size:
+                    state_batch[c] = state
+                    next_state_batch[c] = torch.from_numpy(next_state)
+
+                    action_batch[c] = action
+                    reward_batch[c] = reward
+                    done_batch[c] = done
+                    c += 1
+
+                    if done:
+                        break
+
+                    continue
+
+
+                elif c >= mem_size:
+                    idx = c - (mem_size * (c // mem_size))
+
+                    state_batch[idx] = state
+                    next_state_batch[idx] = torch.from_numpy(next_state)
+
+                    action_batch[idx] = action
+                    reward_batch[idx] = reward
+                    done_batch[idx] = done
+
+                else:
+                    state_batch = torch.cat((state_batch, torch.unsqueeze(state, dim=0)), dim=0)
+
+                    next_state = torch.from_numpy(next_state)
+                    next_state_batch = torch.cat((next_state_batch, torch.unsqueeze(next_state, dim=0)), dim=0)
+
+                    action_batch = torch.cat((action_batch, torch.unsqueeze(torch.tensor([action]), dim=0)), dim=0)
+                    reward_batch = torch.cat((reward_batch, torch.unsqueeze(torch.Tensor([reward]), dim=0)), dim=0)
+                    done_batch = torch.cat((done_batch, torch.unsqueeze(torch.Tensor([done]), dim=0)), dim=0)
+
+                c += 1
+                random_indices = torch.randperm(action_batch.size(0))[:batch_size]
+
+                q_vals = self.model(state_batch[random_indices])
+
+                with torch.no_grad():
+                    next_q_vals = self.model(next_state_batch[random_indices])
+
+                y_hat = reward_batch[random_indices].squeeze() + 0.99 * (
+                            (1 - done_batch[random_indices]).squeeze() * torch.max(next_q_vals, dim=1)[0])
+                y = q_vals.gather(dim=1, index=action_batch[random_indices].long()).squeeze()
+
+                loss = self.loss_func(y, y_hat)
+                losses.append(loss.item() / batch_size)
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.optimizer.step()
+
+                if done:
+                    break
+
+        if epsilon > self.min_eps:
+            epsilon -= (1 / 1000)
+
+            # Print the progress
+        if ((c % np.round(epochs / 10) == 0)):
+            error = np.mean(losses)
+            print(f'epoch: {c:d}, error: {error:.2f}')
+        plt.plot(losses)
+
+
 def main():
-    a = QDeepLearningExpReplay(1e-3, 0.99, 1, 0.001)
-    a.train(epochs=1900)
-    a.test(num_games=100)
+    # a = QDeepLearningExpReplay(1e-3, 0.99, 1, 0.001)
+    # a.train(epochs=300)
+    # print(np.mean(a.test(num_games=100)))
+
+    b = QDeepLearningExpReplay(1e-3, 0.99, 1, 0.001)
+    b.train_torch(epochs=300)
+    print("----")
+    print(np.mean(b.test(num_games=100)))
 
 
 if __name__ == "__main__":
